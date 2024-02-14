4739109b9d246de8fbb50a44ecc1b84bd87f72c0
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-14 16:01:19 -0500

    Added html elements, logic for operate fn
    imported from my codepen

---
+++ script.js
@@ -0,0 +1,40 @@
+//global state & node references
+const calcFrame = document.querySelector('.frame');
+
+let calculator = {
+	add: (num1,num2)=> {
+		return num1 + num2;
+	},
+	subtract: (num1,num2)=> {
+		return num1 - num2;
+	},
+	multiply: (num1,num2)=> {
+		return num1 * num2;
+	},
+	divide: (num1,num2)=> {
+		return num1 / num2;
+	},
+	//string format '1+2' =turns into=> ['1','+','2']
+  //regular function so 'this' context is the calculator object 
+	operate: function (inputStr) {
+		//isNaN(item) answers: "is the item NaN when used in a number context (coercion)"?
+		//below checks if an item in the array coerces to NaN, and returns the item (i.e. the operator), or the coerced number.
+		const [num1, oper, num2] = inputStr.split('').map( item=> isNaN(item) ? item : +item );
+		switch (oper) {
+			case '+':
+				return this.add(num1,num2).toFixed(6);
+			case '-':
+				return this.subtract(num1,num2).toFixed(6);
+			case '*':
+				return this.multiply(num1,num2).toFixed(6);
+			case '/':
+				return this.divide(num1,num2).toFixed(6);
+		}
+	}
+}
+
+console.log( calculator.operate('2/3') )
+
+
+
+
