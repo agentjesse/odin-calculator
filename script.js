966127f5d79b03a4067580b2ede964a07010b254
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-14 17:11:03 -0500

    Fixed calculator styling with flexbox

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ let calculator = {
 	divide: (num1,num2)=> {
 		return num1 / num2;
 	},
+  testStateVal: null,
 	//string format '1+2' =turns into=> ['1','+','2']
   //regular function so 'this' context is the calculator object 
 	operate: function (inputStr) {
+    this.testStateVal = 'poop'
 		//isNaN(item) answers: "is the item NaN when used in a number context (coercion)"?
 		//below checks if an item in the array coerces to NaN, and returns the item (i.e. the operator), or the coerced number.
 		const [num1, oper, num2] = inputStr.split('').map( item=> isNaN(item) ? item : +item );
@@ -30,10 +32,12 @@ let calculator = {
 			case '/':
 				return this.divide(num1,num2).toFixed(6);
 		}
+    
 	}
 }
 
-console.log( calculator.operate('2/3') )
+console.log( calculator.operate('2*3') )
+console.log( calculator.testStateVal )
 
 
 
