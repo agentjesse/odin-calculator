016179a6b1e25855dd0a21197bfc90dd5ac32f7f
jesse <31576321+agentjesse@users.noreply.github.com>
2024-02-14 20:57:42 -0500

    Added screen update logic for digits and decimal

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //global state & node references
-const calcFrame = document.querySelector('.frame');
+const screen = document.querySelector('.screen');
+let screenData = '';
+let decimalAdded = false;
 
-let calculator = {
+let calculationHandle = {
 	add: (num1,num2)=> {
 		return num1 + num2;
 	},
@@ -14,11 +16,9 @@ let calculator = {
 	divide: (num1,num2)=> {
 		return num1 / num2;
 	},
-  testStateVal: null,
 	//string format '1+2' =turns into=> ['1','+','2']
-  //regular function so 'this' context is the calculator object 
+  //regular function so 'this' context is the calculationHandle object 
 	operate: function (inputStr) {
-    this.testStateVal = 'poop'
 		//isNaN(item) answers: "is the item NaN when used in a number context (coercion)"?
 		//below checks if an item in the array coerces to NaN, and returns the item (i.e. the operator), or the coerced number.
 		const [num1, oper, num2] = inputStr.split('').map( item=> isNaN(item) ? item : +item );
@@ -32,13 +32,35 @@ let calculator = {
 			case '/':
 				return this.divide(num1,num2).toFixed(6);
 		}
-    
-	}
+	},
+  //state variables. need to keep track of last calculation for display and processing.
+  lastCalc: null,
+}
+console.log( `calculationHandle test: 2*3= `, calculationHandle.operate('2*3') ) //test with SINGLE INTEGERS and an operand
+
+//display update logic: fully replace data annd store the state
+const updateScreen = newData=> {
+  screen.textContent = newData;
+  screenData = newData;
 }
 
-console.log( calculator.operate('2*3') )
-console.log( calculator.testStateVal )
-
+//button handling logic. store numbers until an operator or equal button is pressed, then use calculationHandle object.
+document.querySelector('.frame').addEventListener('click',e=>{
+  e.stopPropagation();
+  const pressedBtn = e.target.dataset.id; // ac,=,+,-,/,*,0-9 string
+  //send digit presses to screen
+  switch (pressedBtn){
+    case '.':
+      if (!decimalAdded){
+        updateScreen(screenData+pressedBtn);
+        decimalAdded = true;
+      }
+      break;
+    case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
+      updateScreen(screenData+pressedBtn);
+  }
+  
+})
 
 
 
